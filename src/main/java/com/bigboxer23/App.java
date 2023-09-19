9a44d274d7da73ede532e54cf6c6bf72579c793d
Matt Jones <bigboxer23@gmail.com>
2023-09-19 10:07:18 -0500

    Initial commit

---
+++ src/main/java/com/bigboxer23/App.java
@@ -0,0 +1,13 @@
+package com.bigboxer23;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    }
+}
---
+++ src/main/java/com/bigboxer23/LambdaFunction.java
@@ -0,0 +1,11 @@
+package com.bigboxer23;
+
+/**
+ *
+ */
+public class LambdaFunction
+{
+	public void hello() {
+		System.out.println("Hello Lambda");
+	}
+}
