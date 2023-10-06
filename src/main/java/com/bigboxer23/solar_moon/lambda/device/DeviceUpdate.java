e4ffd0f83dc11eef01f3526b96d183583214ecd2
Matt Jones <bigboxer23@gmail.com>
2023-10-06 14:56:41 -0500

    adding lambda methods for add/delete/update

--- src/main/java/com/bigboxer23/solar_moon/lambda/device/DeviceUpdate.java
+++ src/main/java/com/bigboxer23/solar_moon/lambda/device/DeviceUpdate.java
@@ -1,14 +1,23 @@
 package com.bigboxer23.solar_moon.lambda.device;
 
+import com.bigboxer23.solar_moon.data.Device;
 import com.bigboxer23.solar_moon.lambda.MethodHandler;
 import com.bigboxer23.solar_moon.lambda.data.LambdaRequest;
 import com.bigboxer23.solar_moon.lambda.data.LambdaResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 /** */
 public class DeviceUpdate extends MethodHandler {
 	@Override
 	public LambdaResponse handleLambdaRequest(LambdaRequest request) throws IOException {
-		return null;
+		return Optional.ofNullable(moshi.adapter(Device.class).fromJson(request.getBody()))
+				.map(device -> {
+					device.setClientId(getCustomerIdFromRequest(request));
+					deviceComponent.updateDevice(device);
+					return DeviceGet.getDeviceResponse(deviceComponent.getDevice(device.getId(), device.getClientId()));
+				})
+				.orElseGet(
+						() -> new LambdaResponse(NOT_FOUND, "Could not find device to update", APPLICATION_JSON_VALUE));
 	}
 }
