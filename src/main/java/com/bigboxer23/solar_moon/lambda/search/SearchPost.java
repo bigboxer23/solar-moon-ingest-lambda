4a907ea76fadf10519ddd98bcc7a434a1ffb3297
Matt Jones <bigboxer23@gmail.com>
2023-10-18 14:45:25 -0500

    Don't search twice... (#19)

--- src/main/java/com/bigboxer23/solar_moon/lambda/search/SearchPost.java
+++ src/main/java/com/bigboxer23/solar_moon/lambda/search/SearchPost.java
@@ -3,19 +3,26 @@ package com.bigboxer23.solar_moon.lambda.search;
 import com.bigboxer23.solar_moon.lambda.MethodHandler;
 import com.bigboxer23.solar_moon.lambda.data.LambdaRequest;
 import com.bigboxer23.solar_moon.lambda.data.LambdaResponse;
+import com.bigboxer23.solar_moon.lambda.utils.PropertyUtils;
 import com.bigboxer23.solar_moon.open_search.OpenSearchUtils;
 import com.bigboxer23.solar_moon.open_search.SearchJSON;
 import java.io.IOException;
 import java.util.Optional;
+import software.amazon.awssdk.utils.StringUtils;
 
 /** */
 public class SearchPost extends MethodHandler {
+
+	private String customerIdOverride = PropertyUtils.getProperty("customer.id.override");
+
 	@Override
 	public LambdaResponse handleLambdaRequest(LambdaRequest request) throws IOException {
 		return Optional.ofNullable(moshi.adapter(SearchJSON.class).fromJson(request.getBody()))
 				.map(searchJSON -> {
-					searchJSON.setCustomerId(getCustomerIdFromRequest(request));
-					OSComponent.search(searchJSON);
+					searchJSON.setCustomerId(
+							StringUtils.isBlank(customerIdOverride)
+									? getCustomerIdFromRequest(request)
+									: customerIdOverride);
 					return new LambdaResponse(
 							OK, OpenSearchUtils.queryToJson(OSComponent.search(searchJSON)), APPLICATION_JSON_VALUE);
 				})
