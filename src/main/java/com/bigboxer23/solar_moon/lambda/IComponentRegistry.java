a563cf66f99a58ca7699f8dd8f4aa420f913f497
Matt Jones <bigboxer23@gmail.com>
2023-10-30 10:57:45 -0500

    add subscription creation on user creation

--- src/main/java/com/bigboxer23/solar_moon/lambda/IComponentRegistry.java
+++ src/main/java/com/bigboxer23/solar_moon/lambda/IComponentRegistry.java
@@ -2,8 +2,10 @@ package com.bigboxer23.solar_moon.lambda;
 
 import com.bigboxer23.solar_moon.CustomerComponent;
 import com.bigboxer23.solar_moon.DeviceComponent;
+import com.bigboxer23.solar_moon.SubscriptionComponent;
 import com.bigboxer23.solar_moon.open_search.OpenSearchComponent;
 import com.squareup.moshi.Moshi;
+import jdk.jshell.JShell;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +16,8 @@ public interface IComponentRegistry {
 	DeviceComponent deviceComponent = new DeviceComponent();
 
 	OpenSearchComponent OSComponent = new OpenSearchComponent();
+
+	SubscriptionComponent subscriptionComponent = new SubscriptionComponent();
 
 	Logger logger = LoggerFactory.getLogger(IComponentRegistry.class);
 }
