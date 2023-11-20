c28cfbbcccea78a0c1d163509576847bdaf35a08
Matt Jones <bigboxer23@gmail.com>
2023-11-20 10:57:40 -0600

    update function scripts to output better

--- scripts/functions/customer.sh
+++ scripts/functions/customer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 mvn package -DskipTests
-aws lambda update-function-code --function-name customer --region us-west-2 --s3-bucket solarmoonanalytics --s3-key lambda/solar-moon-ingest-lambda-0.1.jar
+aws lambda update-function-code --function-name customer --region us-west-2 --s3-bucket solarmoonanalytics --s3-key lambda/solar-moon-ingest-lambda-0.1.jar --output text
