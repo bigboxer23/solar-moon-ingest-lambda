c3d1e59b7439a244955a4670e7dde1d428a86c04
Matt Jones <bigboxer23@gmail.com>
2023-11-30 15:13:21 -0600

    Adding lambda function for sending batched notifications (#49)

--- scripts/functions/alarm.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-mvn package -DskipTests
-aws lambda update-function-code --function-name alarm --region us-west-2 --s3-bucket solarmoonanalytics --s3-key lambda/solar-moon-ingest-lambda-0.1.jar --output text
