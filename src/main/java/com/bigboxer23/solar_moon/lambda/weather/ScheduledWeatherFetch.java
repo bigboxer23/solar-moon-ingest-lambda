5ce61afc8e4b62d266b4fa5d6a2ff57abfabbfa7
Matt Jones <bigboxer23@gmail.com>
2023-12-02 12:11:35 -0600

    add lambda to fetch weather data

---
+++ src/main/java/com/bigboxer23/solar_moon/lambda/weather/ScheduledWeatherFetch.java
@@ -0,0 +1,30 @@
+package com.bigboxer23.solar_moon.lambda.weather;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.bigboxer23.solar_moon.lambda.AbstractRequestStreamHandler;
+import com.bigboxer23.solar_moon.lambda.data.LambdaRequest;
+import com.bigboxer23.solar_moon.lambda.data.LambdaResponse;
+import com.bigboxer23.solar_moon.web.TransactionUtil;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ *
+ */
+public class ScheduledWeatherFetch extends AbstractRequestStreamHandler
+{
+	@Override
+	public LambdaResponse handleLambdaRequest(LambdaRequest request) throws IOException
+	{
+		return null;
+	}
+
+	@Override
+	public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) {
+		TransactionUtil.updateServiceCalled(getClass().getSimpleName());
+		weatherComponent.fetchNewWeather();
+		after();
+	}
+}
