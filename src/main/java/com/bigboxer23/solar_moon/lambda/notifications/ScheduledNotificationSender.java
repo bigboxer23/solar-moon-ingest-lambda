dd7419f37ea75fac1a03936c6ca8a4890ed8a601
Matt Jones <bigboxer23@gmail.com>
2024-04-03 12:11:04 -0500

    fix: add better error handling for non-standard responding lambdas (eg scheduled or sqs triggered) (#85)

--- src/main/java/com/bigboxer23/solar_moon/lambda/notifications/ScheduledNotificationSender.java
+++ src/main/java/com/bigboxer23/solar_moon/lambda/notifications/ScheduledNotificationSender.java
@@ -4,7 +4,6 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.bigboxer23.solar_moon.lambda.AbstractRequestStreamHandler;
 import com.bigboxer23.solar_moon.lambda.data.LambdaRequest;
 import com.bigboxer23.solar_moon.lambda.data.LambdaResponse;
-import com.bigboxer23.solar_moon.web.TransactionUtil;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -18,8 +17,9 @@ public class ScheduledNotificationSender extends AbstractRequestStreamHandler {
 
 	@Override
 	public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) {
-		TransactionUtil.updateServiceCalled(getClass().getSimpleName());
-		alarmComponent.sendPendingNotifications();
-		after();
+		safeHandleRequest(() -> {
+			alarmComponent.sendPendingNotifications();
+			return null;
+		});
 	}
 }
