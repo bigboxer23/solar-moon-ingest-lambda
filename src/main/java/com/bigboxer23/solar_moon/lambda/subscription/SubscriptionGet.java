981d01b647ed768f3c12cd035ee3b3e994beae72
Matt Jones <bigboxer23@gmail.com>
2024-04-24 14:27:37 -0500

    feat: add support for trial usage

--- src/main/java/com/bigboxer23/solar_moon/lambda/subscription/SubscriptionGet.java
+++ src/main/java/com/bigboxer23/solar_moon/lambda/subscription/SubscriptionGet.java
@@ -1,5 +1,6 @@
 package com.bigboxer23.solar_moon.lambda.subscription;
 
+import com.bigboxer23.solar_moon.data.Subscription;
 import com.bigboxer23.solar_moon.lambda.MethodHandler;
 import com.bigboxer23.solar_moon.lambda.data.LambdaRequest;
 import com.bigboxer23.solar_moon.lambda.data.LambdaResponse;
@@ -11,7 +12,11 @@ public class SubscriptionGet extends MethodHandler {
 	public LambdaResponse handleLambdaRequest(LambdaRequest request) throws IOException {
 		return new LambdaResponse(
 				OK,
-				subscriptionComponent.getSubscriptionPacks(getCustomerIdFromRequest(request)) + "",
+				subscriptionComponent
+								.getSubscription(getCustomerIdFromRequest(request))
+								.map(Subscription::getPacks)
+								.orElse(0)
+						+ "",
 				APPLICATION_JSON_VALUE);
 	}
 }
