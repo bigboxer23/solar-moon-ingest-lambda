8fb6d7d2c02ea14911c71842cc6af9d100cb569d
Matt Jones <bigboxer23@gmail.com>
2024-05-01 12:55:40 -0500

    ops: slightly refactor alert and subscription git to avoid NPE, passing Optional (#97)

--- src/main/java/com/bigboxer23/solar_moon/lambda/billing/GetSubscriptions.java
+++ src/main/java/com/bigboxer23/solar_moon/lambda/billing/GetSubscriptions.java
@@ -10,6 +10,7 @@ import com.squareup.moshi.Types;
 import com.stripe.exception.StripeException;
 import java.io.IOException;
 import java.util.List;
+import software.amazon.awssdk.utils.StringUtils;
 
 /** */
 public class GetSubscriptions extends MethodHandler {
@@ -17,13 +18,18 @@ public class GetSubscriptions extends MethodHandler {
 
 	@Override
 	public LambdaResponse handleLambdaRequest(LambdaRequest request) throws IOException {
+		String stripeCustomerId = getCustomerFromRequest(request)
+				.map(Customer::getStripeCustomerId)
+				.orElse(null);
+		if (StringUtils.isEmpty(stripeCustomerId)) {
+			logger.warn("GetSubscriptions: no stripe customer Id");
+			return new LambdaResponse(BAD_REQUEST, "error retrieving subscriptions");
+		}
 		try {
 			return new LambdaResponse(
 					OK,
 					moshi.adapter(Types.newParameterizedType(List.class, SubscriptionPriceInfo.class))
-							.toJson(component.getActiveSubscriptionPriceInfo(getCustomerFromRequest(request)
-									.map(Customer::getStripeCustomerId)
-									.orElse(null))),
+							.toJson(component.getActiveSubscriptionPriceInfo(stripeCustomerId)),
 					APPLICATION_JSON_VALUE);
 		} catch (StripeException e) {
 			logger.warn("GetSubscriptions", e);
