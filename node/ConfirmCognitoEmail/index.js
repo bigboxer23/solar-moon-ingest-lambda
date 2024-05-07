663a3346b492bddbca863c969865fd1ea7d899aa
Matt Jones <bigboxer23@gmail.com>
2024-05-07 10:58:19 -0500

    chore: update confirmation lambda to node v20 runtime (#99)

--- node/ConfirmCognitoEmail/index.js
+++ node/ConfirmCognitoEmail/index.js
@@ -1,47 +1,39 @@
-'use strict';
-var AWS = require('aws-sdk');
-AWS.config.setPromisesDependency(require('bluebird'));
-var CognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider({
-	apiVersion: '2019-11-07',
-	region: process.env.REGION
-});
-exports.handler = (req, context, callback) => {
-	console.log(req);
-	const confirmationCode = req.queryStringParameters.code;
-	const username = req.queryStringParameters.username;
-	const clientId = req.queryStringParameters.clientId;
-	let params = {
-		ClientId: clientId,
-		ConfirmationCode: confirmationCode,
-		Username: username
-	};
-	//Validating the user
-	let confirmSignUp = CognitoIdentityServiceProvider.confirmSignUp(params).promise();
-	confirmSignUp.then(
-			(data) => {
-				const response = {
-					"statusCode": 301,
-					"headers": {
-						"location": "https://solarmoonanalytics.com/verified"
-					},
-					"body": "",
-					"isBase64Encoded": false
-				};
-				callback(null, response);
-			}
-	).catch(
-			(error) => {
-				console.log(error.message);
-				const response = {
-					"statusCode": 301,
-					"headers": {
-						"location": "https://solarmoonanalytics.com/verified"
-					},
-					"body": "",
-					"isBase64Encoded": false
-				};
+import {
+	ConfirmSignUpCommand,
+	CognitoIdentityProviderClient,
+} from "@aws-sdk/client-cognito-identity-provider";
 
-				return callback(null, response);
-			}
-	)
+export const handler = async (event) => {
+
+	const client = new CognitoIdentityProviderClient({});
+	const command = new ConfirmSignUpCommand({
+		ClientId: event.queryStringParameters.clientId,
+		ConfirmationCode: event.queryStringParameters.code,
+		Username: event.queryStringParameters.username
+	});
+	try {
+		const ret = await client.send(command);
+		console.log('ret: ' + JSON.stringify(ret));
+		if (ret["$metadata"].httpStatusCode === 200) {
+			return {
+				"statusCode": 301,
+				"headers": {
+					"location": "https://solarmoonanalytics.com/verified"
+				},
+				"body": "",
+				"isBase64Encoded": false
+			};
+		}
+	} catch( e) {
+		console.log('exception: ' + e);
+		console.log(JSON.stringify(e));
+	}
+	return {
+		"statusCode": 301,
+		"headers": {
+			"location": "https://solarmoonanalytics.com/accountActivationError"
+		},
+		"body": "",
+		"isBase64Encoded": false
+	};
 };
