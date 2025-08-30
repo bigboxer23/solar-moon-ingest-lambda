c1a7f15f97abc0f9d1d643e94de8d3b3486ab8cb
Matt Jones <bigboxer23@gmail.com>
2025-08-29 21:01:49 -0500

    feat: adding log monitor lambda (#289)

---
+++ src/main/java/com/bigboxer23/solar_moon/lambda/ops/ScheduledErrorLogMonitor.java
@@ -0,0 +1,27 @@
+package com.bigboxer23.solar_moon.lambda.ops;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.bigboxer23.solar_moon.lambda.AbstractRequestStreamHandler;
+import com.bigboxer23.solar_moon.lambda.data.LambdaRequest;
+import com.bigboxer23.solar_moon.lambda.data.LambdaResponse;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Calls component that looks for concerning logged messages and reports them to admin
+ */
+public class ScheduledErrorLogMonitor extends AbstractRequestStreamHandler
+{
+	@Override
+	public LambdaResponse handleLambdaRequest(LambdaRequest request) throws IOException
+	{
+		return null;
+	}
+
+	@Override
+	public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) {
+		safeHandleRequest(logMonitorComponent::findAndReportErrorLogging);
+	}
+}
